9a065bd419afbdc3c584616fd5273567109a7b65
Luigi Favaro <luigi.favaro@hotmail.it>
2025-06-02 01:23:10 +0200

    add energy network and configs

--- experiments/calochallenge/model.py
+++ experiments/calochallenge/model.py
@@ -1,12 +1,13 @@
 import math
-import torch
 from einops import rearrange
 
 from FrEIA.framework import InputNode, Node, OutputNode, GraphINN, ConditionNode
-from experiments.base_model import CINN
+from FrEIA.modules import ActNorm
+from experiments.base_model import CINN, BaseModel
 from experiments.calochallenge.freia_utils import (
     get_coupling_block,
     get_permutation_block,
+    get_vit_block_kwargs,
     get_block_kwargs,
 )
 
@@ -27,12 +28,12 @@ class CaloChallengeCINN(CINN):
 
         self.nblocks = nblocks
         self.CouplingBlock = get_coupling_block(coupling_block)
-        self.PermuteBlock = get_permutation_block(is_spatial)
-        self.block_kwargs = get_block_kwargs(
+        self.PermuteBlock = get_permutation_block(nblocks, is_spatial)
+        self.block_kwargs = get_vit_block_kwargs(
+            nblocks,
             is_spatial,
             self.shape,
             self.patch_shape,
-            spatial_factor,
             cinn_kwargs,
             vit_kwargs,
         )
@@ -40,7 +41,7 @@ class CaloChallengeCINN(CINN):
         assert len(self.block_kwargs) == len(self.PermuteBlock)
         assert len(self.block_kwargs) == self.nblocks
 
-        self.build_net()
+        self.net = self.build_net()
 
     def from_patches(self, x, dim=3):
         if dim == 3:
@@ -83,9 +84,7 @@ class CaloChallengeCINN(CINN):
             raise ValueError(dim)
         return x
 
-    def build_net(
-        self,
-    ):
+    def build_net(self):
         self.in_dim = [math.prod(self.num_patches), math.prod(self.patch_shape)]
 
         nodes = [InputNode(*self.in_dim, name="Input")]
@@ -112,4 +111,76 @@ class CaloChallengeCINN(CINN):
         nodes.append(OutputNode([nodes[-1].out0], name="out"))
         nodes.append(cond_node)
 
-        self.net = GraphINN(nodes)
+        return GraphINN(nodes)
+
+
+class CaloChallengeEnergy(BaseModel):
+    def __init__(
+        self,
+        shape,
+        coupling_block,
+        nblocks,
+        cinn_kwargs,
+        subnet_kwargs,
+    ):
+        super().__init__(shape)
+
+        self.nblocks = nblocks
+        self.CouplingBlock = get_coupling_block(coupling_block)
+        self.PermuteBlock = get_permutation_block(nblocks, is_spatial=None)
+        self.block_kwargs = get_block_kwargs(nblocks, cinn_kwargs, subnet_kwargs)
+
+        assert len(self.block_kwargs) == len(self.PermuteBlock)
+        assert len(self.block_kwargs) == self.nblocks
+
+        self.net = self.build_net()
+
+    def build_net(self):
+
+        nodes = [InputNode(*self.shape, name="Input")]
+        cond_node = ConditionNode(1, name="cond")
+        for i in range(self.nblocks):
+            nodes.append(
+                Node(
+                    [nodes[-1].out0],
+                    self.CouplingBlock,
+                    self.block_kwargs[i],
+                    conditions=cond_node,
+                    name=f"block_{i}",
+                )
+            )
+            nodes.append(
+                Node(
+                    [nodes[-1].out0],
+                    self.PermuteBlock[i],
+                    {},
+                    name=f"permute_{i}",
+                )
+            )
+
+        nodes.append(OutputNode([nodes[-1].out0], name="out"))
+        nodes.append(cond_node)
+
+        return GraphINN(nodes)
+
+    def sample_batch(self, batch):
+        """
+        sample from the learned distribution
+
+        Parameters:
+        num_pts (int): Number of samples to generate for each given condition
+        condition (tensor): Conditions
+
+        Returns:
+        tensor[len(condition), num_pts, dims]: Samples
+        """
+        z = torch.normal(
+            0,
+            1,
+            size=(batch.shape[0], *self.shape),
+            device=batch.device,
+            dtype=batch.dtype,
+        )
+        x = super().sample_batch(z, batch)
+
+        return x.reshape(z.shape[0], *self.shape)
--- nn/subnets.py
+++ nn/subnets.py
@@ -28,3 +28,43 @@ class SubnetViT(nn.Module):
         t = torch.ones(len(x), 1, dtype=torch.float, device=x.device)  # TODO
         vit_output = self.vit(x.to(torch.float32), t, c[0].to(torch.float32))
         return vit_output
+
+
+class SubnetMLP(nn.Module):
+    "A subnet constructor for a MLP"
+
+    def __init__(
+        self,
+        x_in=None,
+        x_out=None,
+        subnet_kwargs=None,
+    ):
+        super().__init__()
+
+        defaults = {
+            "n_layers": 2,
+            "hidden_channels": [128, 128],
+            "dropout": 0.0,
+        }
+
+        self.x_in = x_in
+        self.x_out = x_out
+        for k, p in defaults.items():
+            setattr(self, k, subnet_kwargs[k] if k in subnet_kwargs else p)
+
+        self.layers = []
+        self.layers.append(nn.Linear(self.x_in, self.hidden_channels[0]))
+        self.layers.append(nn.ReLU())
+        for n in range(self.n_layers - 1):
+            self.layers.append(
+                nn.Linear(self.hidden_channels[n], self.hidden_channels[n + 1])
+            )
+            if self.dropout > 0:
+                self.layers.append(nn.Dropout(p=self.dropout))
+            self.layers.append(nn.ReLU())
+        self.layers.append(nn.Linear(self.hidden_channels[n], self.x_out))
+
+        self.mlp = nn.Sequential(*self.layers)
+
+    def forward(self, x):
+        return self.mlp(x)
